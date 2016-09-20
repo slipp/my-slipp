7e808d015a8cdb868e3e0e6f5c440a36467d25d0
javajigi <javajigi@gmail.com>
2016-09-20 18:08:51 +0900

    initial commit

---
+++ src/main/java/net/slipp/MySlippApplication.java
@@ -0,0 +1,12 @@
+package net.slipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySlippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySlippApplication.class, args);
+	}
+}
