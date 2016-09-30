39890a05300079dfe6d357781fc10d4672c81f30
javajigi <javajigi@gmail.com>
2016-09-30 14:41:24 +0900

    새로운 html 적용, url 리팩토링, html 중복 제거

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -5,26 +5,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.slipp.domain.User;
 import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@PostMapping("")
 	public String create(User user) {
 		System.out.println("user : " + user);
 		userRepository.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";
 	}
 }
