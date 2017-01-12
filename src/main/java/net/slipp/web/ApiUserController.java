80dc091438c327be5b6b3728abe3e07d9abfe50e
javajigi <javajigi@gmail.com>
2017-01-12 11:36:59 +0900

    Restful API 기반으로 구현함.

--- src/main/java/net/slipp/web/ApiUserController.java
+++ src/main/java/net/slipp/web/ApiUserController.java
@@ -1,8 +1,13 @@
 package net.slipp.web;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +21,13 @@ public class ApiUserController {
 	private UserRepository userRepository;
 	
 	@GetMapping("/{id}")
-	public User show(@PathVariable Long id) {
-		return userRepository.findOne(id);
+	public ResponseEntity<User> show(@PathVariable Long id) {
+		return ResponseEntity.ok(userRepository.findOne(id));
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<?> create(@RequestBody User user) throws Exception {
+		User result = userRepository.save(user);
+		return ResponseEntity.created(new URI("/api/users/" + result.getId())).build();
 	}
 }
